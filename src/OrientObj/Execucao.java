a8dad32f2b7a335190d6478ea381272ad4b72807
chrispittersl <christopher.silva17@etec.sp.gov.br>
2019-04-23 22:50:04 -0300

    OrientacaoAObjetos

---
+++ src/OrientObj/Execucao.java
@@ -0,0 +1,13 @@
+package OrientObj;
+
+import OrientObj.Pessoa;
+
+public class Execucao {
+
+	public static void main(String[] args) {
+		Pessoa ps = new Pessoa();
+		ps.andar();
+		ps.respirar();
+	}
+
+}
---
+++ src/OrientObj/Pessoa.java
@@ -0,0 +1,22 @@
+package OrientObj;
+
+import javax.swing.JOptionPane;
+
+public class Pessoa {
+	
+	public void andar() {
+		JOptionPane.showMessageDialog(null,"Andando");
+	}
+	protected void respirar() {
+		JOptionPane.showMessageDialog(null,"Respirando");
+	}
+	private void dormir() {// Este método só será utilizado dentro desta classe, pois ese método é private(privado)
+		JOptionPane.showMessageDialog(null,"Dormindo");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
